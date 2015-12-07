bc909f42afee2e8f01f2abccb1a4d3a7ddd1b5cd
Rex <503431920@qq.com>
2015-12-07 17:27:20 +0800

    add news

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,33 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope, $ionicActionSheet) {
+    // 点击按钮触发，或一些其他的触发条件
+    $scope.show = function() {
+        console.log(123123123);
+
+        // 显示操作表
+        $ionicActionSheet.show({
+            buttons: [{
+                text: '<b>Share</b> This'
+            }, {
+                text: 'Move'
+            }, ],
+            destructiveText: 'Delete',
+            titleText: 'Modify your album',
+            cancelText: 'Cancel',
+            buttonClicked: function(index) {
+                if (index == 0) {
+                    console.log("Share This");
+                }
+                if (index == 1) {
+                    console.log("Move");
+                }
+                return true;
+            }
+        });
+
+    };
+})
 
 .controller('ChatsCtrl', function($scope, Chats) {
     // With the new view caching in Ionic, Controllers are only called
@@ -21,6 +48,48 @@ angular.module('starter.controllers', [])
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
         $scope.chat = Chats.get($stateParams.chatId);
+    })
+    .controller('NewsCtrl', function($scope, $ionicLoading) {
+
+        $scope.data = {
+            showDelete: false
+        };
+
+        $scope.edit = function(item) {
+            alert('Edit Item: ' + item.id);
+        };
+        $scope.share = function(item) {
+            alert('Share Item: ' + item.id);
+        };
+
+        $scope.moveItem = function(item, fromIndex, toIndex) {
+            $scope.items.splice(fromIndex, 1);
+            $scope.items.splice(toIndex, 0, item);
+        };
+
+        $scope.onItemDelete = function(item) {
+            $scope.items.splice($scope.items.indexOf(item), 1);
+        };
+
+        $.ajax({
+                url: 'http://localhost:8070/news/getNewsCollection',
+                type: "GET",
+                contentType: "application/json; charset=utf-8",
+                dataType: "jsonp",
+                data: {},
+            })
+            .done(function(response) {
+                console.log("success");
+                $(".has-header").css('top', '90px');
+                $scope.items = response;
+                console.log(JSON.stringify(response));
+            })
+            .fail(function() {
+                console.log("error");
+            })
+            .always(function() {
+                console.log("complete");
+            });
     })
 
 .controller('AccountCtrl', function($scope) {
