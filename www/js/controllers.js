9b74ceae80881ae1c92ff0ffd49e215f12df951d
wangrex <503431920@qq.com>
2015-12-21 21:39:33 +0800

    add match

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ['ionic'])
 
 .controller('DashCtrl', function($scope, $ionicActionSheet) {
     // 点击按钮触发，或一些其他的触发条件
@@ -92,7 +92,7 @@ angular.module('starter.controllers', [])
     };
 
     $scope.loadMore = function() {
-        News.all(skip,3).then(function(data) {
+        News.all(skip, 3).then(function(data) {
 
             if (data.length == 0) {
                 $scope.moreDataCanBeLoaded = false;
@@ -111,7 +111,7 @@ angular.module('starter.controllers', [])
     };
 
     // get data on page load
-    News.all(0,3).then(function(data) {
+    News.all(0, 3).then(function(data) {
         $scope.items = data;
         skip = $scope.items.length;
     }, function(data) { // 处理错误 .reject  
@@ -158,6 +158,100 @@ angular.module('starter.controllers', [])
         };
     });
     // $scope.news = News.get($stateParams.newsId);
+
+})
+
+.controller('MatchCtrl', function($scope, $stateParams, Match, $ionicPopup) {
+    $scope.dateModel = new Date("08-14-2015");
+    $scope.datePopup = new Date("08-14-2015");
+    var disabledDates = [
+        new Date(1437719836326),
+        new Date(2015, 7, 10), //months are 0-based, this is August, 10th!
+        new Date('Wednesday, August 12, 2015'), //Works with any valid Date formats like long format
+        new Date("08-14-2015"), //Short format
+        new Date(1439676000000) //UNIX format
+    ];
+    var monthList = ["一月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "十一月", "十二月"];
+    var weekDaysList = ["日", "一", "二", "三", "四", "五", "六"];
+    $scope.datepickerObject = {};
+    $scope.datepickerObject.inputDate = new Date();
+
+    $scope.datepickerObjectModal = {
+        titleLabel: '选择日期', //Optional
+        todayLabel: '今天', //Optional
+        closeLabel: '关闭', //Optional
+        setLabel: '选择', //Optional
+        errorMsgLabel: '请选择时间.', //Optional
+        setButtonType: 'button-assertive', //Optional
+        modalHeaderColor: 'bar-positive', //Optional
+        modalFooterColor: 'bar-positive', //Optional
+        templateType: 'modal', //Optional
+        inputDate: $scope.datepickerObject.inputDate, //Optional
+        mondayFirst: true, //Optional
+        disabledDates: disabledDates, //Optional
+        monthList: monthList, //Optional
+        weekDaysList: weekDaysList,
+        from: new Date(2012, 5, 1), //Optional
+        to: new Date(2016, 7, 1), //Optional
+        callback: function(val) { //Optional
+            datePickerCallbackModal(val);
+        }
+    };
+    $scope.datepickerObjectPopup = {
+        titleLabel: '选择日期', //Optional
+        todayLabel: '今天', //Optional
+        closeLabel: '关闭', //Optional
+        setLabel: '选择', //Optional
+        errorMsgLabel: '请选择时间.', //Optional
+        setButtonType: 'button-assertive', //Optional
+        modalHeaderColor: 'bar-positive', //Optional
+        modalFooterColor: 'bar-positive', //Optional
+        templateType: 'popup', //Optional
+        inputDate: $scope.datepickerObject.inputDate, //Optional
+        //mondayFirst: true, //Optional
+        sundayFirst: true, //Optional
+        disabledDates: disabledDates, //Optional
+        monthList: monthList, //Optional
+        weekDaysList: weekDaysList,
+        from: new Date(2014, 5, 1), //Optional
+        to: new Date(2016, 7, 1), //Optional
+        callback: function(val) { //Optional
+            datePickerCallbackPopup(val);
+        }
+    };
+
+    var datePickerCallbackModal = function(val) {
+        if (typeof(val) === 'undefined') {
+            console.log('未选择日期');
+        } else {
+            $scope.datepickerObjectModal.inputDate = val;
+            $scope.dateModel = val;
+            console.log('选择的日期是 : ', val)
+        }
+    };
+
+    var datePickerCallbackPopup = function(val) {
+        if (typeof(val) === 'undefined') {
+            console.log('未选择日期');
+        } else {
+            $scope.datepickerObjectPopup.inputDate = val;
+            $scope.datePopup = val;
+            console.log('选择的日期是 : ', val)
+        }
+    };
+
+    /* console.log($stateParams.matchId);
+
+     var promise = Match.get($stateParams.matchId); // 同步调用，获得承诺接口  
+     promise.then(function(data) { // 调用承诺API获取数据 .resolve  
+         //这里有个问题，就是title不能通过这个动态来获取。
+         $scope.match = data[0];
+     }, function(data) { // 处理错误 .reject  
+         $scope.match = {
+             error: '赛事！'
+         };
+     });*/
+    // $scope.news = Match.get($stateParams.matchId);
 
 })
 
--- www/js/services.js
+++ www/js/services.js
@@ -109,6 +109,66 @@ angular.module('starter.services', [])
     };
 })
 
+.factory('Match', function($http, $q) {
+    // Might use a resource here that returns a JSON array
+
+    var Match;
+
+    return {
+        all: function(skip, limit) {
+            var url = 'http://localhost:8070/match/getMatchCollectionRefresh?skip=' + skip + '&limit=' + limit;
+
+            var deferred = $q.defer(); // 声明延后执行，表示要去监控后面的执行  
+            $http({
+                method: 'GET',
+                url: url
+            }).
+            success(function(data, status, headers, config) {
+                deferred.resolve(data); // 声明执行成功，即http请求数据成功，可以返回数据了  
+                Match = data;
+            }).
+            error(function(data, status, headers, config) {
+                deferred.reject(data); // 声明执行失败，即服务器返回错误  
+            });
+            return deferred.promise; // 返回承诺，这里并不是最终数据，而是访问最终数据的API  
+            // end query
+
+        },
+
+        remove: function(match) {
+            match.splice(match.indexOf(match), 1);
+        },
+
+        get: function(matchId) {
+
+            var url = 'http://localhost:8070/match/getMatchDetail?matchId=' + matchId;
+
+            var deferred = $q.defer(); // 声明延后执行，表示要去监控后面的执行  
+            $http({
+                method: 'GET',
+                url: url
+            }).
+            success(function(data, status, headers, config) {
+                deferred.resolve(data); // 声明执行成功，即http请求数据成功，可以返回数据了  
+            }).
+            error(function(data, status, headers, config) {
+                deferred.reject(data); // 声明执行失败，即服务器返回错误  
+            });
+            return deferred.promise; // 返回承诺，这里并不是最终数据，而是访问最终数据的API  
+            // end query
+
+
+            /*console.log(News);
+            for (var i = 0; i < News.length; i++) {
+                if (News[i].newsId === newsId) {
+                    return News[i];
+                }
+            }
+            return null;*/
+        }
+    };
+})
+
 .factory('Results', function() {
     var matches = [{
         id: 0,
