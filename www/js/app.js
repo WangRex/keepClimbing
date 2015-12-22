2a27fc1e45489e950a11682a924dc53e6a7e021c
Rex <503431920@qq.com>
2015-12-22 16:51:11 +0800

    add datepicker

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic', 'ionic-datepicker', 'starter.controllers', 'starter.services'])
 
 .run(function($ionicPlatform) {
     $ionicPlatform.ready(function() {
@@ -97,6 +97,16 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
             'tab-news': {
                 templateUrl: 'templates/news-detail.html',
                 controller: 'NewsDetailCtrl'
+            }
+        }
+    })
+
+    .state('tab.match', {
+        url: '/match',
+        views: {
+            'tab-match': {
+                templateUrl: 'templates/tab-match.html',
+                controller: 'MatchCtrl'
             }
         }
     })
